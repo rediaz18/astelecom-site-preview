62f569b43ccdfbb5d4e1236c5ae42d11dfb91aa7
Rubén Díaz <rediaz1808@gmail.com>
2019-05-08 16:18:31 -0500

    first commit

---
+++ js/js-anim.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+        /*Toggle classes of header*/
+    $(function() {
+        var header = $(".header-1");
+        //var headerIndex = $("#header-index");
+        $(window).scroll(function() {    
+            var scroll = $(window).scrollTop();
+        
+            if(window.location.pathname == "/ASTELECOM/index.html" && scroll>= 650){
+                header.removeClass('header-1').addClass("header-alt");  
+            } else if (scroll >= 250){
+                header.removeClass('header-1').addClass("header-alt");
+            } else {
+                header.removeClass("header-alt").addClass('header-1');
+            }
+
+        });
+    });
+
+     /*Toggle classes of header in INDEX*/
+/*      $(function() {
+        var header = $("#header-index");
+        $(window).scroll(function() {    
+            var scroll = $(window).scrollTop();
+        
+            if (scroll >= 650) {
+                header.removeClass('header-1').addClass("header-alt");
+            } else {
+                header.removeClass("header-alt").addClass('header-1');
+            }
+        });
+    }); */
+
+
+});
