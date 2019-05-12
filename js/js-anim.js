2defa66bc5aa8798978e32e33077174bd2cb8d42
Rubén Díaz <rediaz1808@gmail.com>
2019-05-11 20:34:34 -0500

    second commit
    final astelecom. no colabora, contacto. no imagenes de concurso

--- js/js-anim.js
+++ js/js-anim.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-        /*Toggle classes of header*/
-    $(function() {
+    /*Toggle classes of header*/
+    $(function () {
         var header = $(".header-1");
         //var headerIndex = $("#header-index");
-        $(window).scroll(function() {    
+        $(window).scroll(function () {
             var scroll = $(window).scrollTop();
-        
-            if(window.location.pathname == "/ASTELECOM/index.html" && scroll>= 650){
-                header.removeClass('header-1').addClass("header-alt");  
-            } else if (scroll >= 250){
+
+            if (window.location.pathname == "/ASTELECOM/index.html" && scroll >= 650) {
+                header.removeClass('header-1').addClass("header-alt");
+            } else if (scroll >= 250) {
                 header.removeClass('header-1').addClass("header-alt");
             } else {
                 header.removeClass("header-alt").addClass('header-1');
@@ -18,19 +18,19 @@ $(document).ready(function(){
         });
     });
 
-     /*Toggle classes of header in INDEX*/
-/*      $(function() {
-        var header = $("#header-index");
-        $(window).scroll(function() {    
-            var scroll = $(window).scrollTop();
-        
-            if (scroll >= 650) {
-                header.removeClass('header-1').addClass("header-alt");
-            } else {
-                header.removeClass("header-alt").addClass('header-1');
-            }
-        });
-    }); */
+    /*Toggle classes of header in INDEX*/
+    /*      $(function() {
+            var header = $("#header-index");
+            $(window).scroll(function() {    
+                var scroll = $(window).scrollTop();
+            
+                if (scroll >= 650) {
+                    header.removeClass('header-1').addClass("header-alt");
+                } else {
+                    header.removeClass("header-alt").addClass('header-1');
+                }
+            });
+        }); */
 
 
 });
